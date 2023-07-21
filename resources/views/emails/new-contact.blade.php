9fa1842a39416e403e2cd874363d7402e615d5e2
MathiasChirivi <chirivimathias3@gmail.com>
2023-07-21 19:12:34 +0200

    add request_Mail

---
+++ resources/views/emails/new-contact.blade.php
@@ -0,0 +1,8 @@
+<h1>Ciao Amministatore</h1>
+
+<p>
+    Hai ricevuto una richiesta di contatto dal tuo sito, ecco i dettagli:
+    <div>Nome: {{ $contactData["name"] }}</div>
+    <div>Indirizzo Email: {{ $contactData["email"] }}</div>
+    <div>Messaggio: {{ $contactData["message"] }}</div>
+</p>
